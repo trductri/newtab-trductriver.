b0a449f33db40234b29f0722f34ed1a87cec8615
Trinh Duc Tri <39829382+trductri@users.noreply.github.com>
2018-06-20 12:38:05 +0700

    Update script.js

--- script.js
+++ script.js
@@ -7,8 +7,8 @@ const calcTime = () => {
 			hours = date.getHours(),
 			mins  = date.getMinutes()
 
-	hours > 12 && (sal = "afternoon")
-	hours > 18 && (sal = "evening")
+	hours > 11 && (sal = "afternoon")
+	hours > 17 && (sal = "evening")
 	hours > 21 && (sal = "night")
 	hours > 11 && hours < 24 && (ampm = "PM")
 	hours > 12 && (hours -= 12)
